4e8cf2d3886e4863baf7b8d3aaea6ff0b3690cd0
Spoorthi <118953638+spoorthid9@users.noreply.github.com>
2022-12-03 20:30:52 +0530

    Update yearwise.sql

--- yearwise.sql
+++ yearwise.sql
@@ -5,6 +5,3 @@ FROM layoffsproj.layoffsrawdata
 WHERE industry != ''
 ORDER BY YearLaidOff
 
-Result (showing few records only) :
-
-![yearwise](https://user-images.githubusercontent.com/118953638/205447032-72ced8f6-4d72-47e0-94cc-49c009c55862.JPG)
