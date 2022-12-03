474b69eb46e6f1b8a5cf7d92066e48933d7a35a0
Spoorthi <118953638+spoorthid9@users.noreply.github.com>
2022-12-03 22:56:39 +0530

    Delete yearwise.sql

--- yearwise.sql
+++
@@ -1,7 +0,0 @@
-#Layoffs along the years
-SELECT DISTINCT(industry),
-EXTRACT(YEAR FROM str_to_date(date,'%d-%m-%Y')) AS YearLaidOff
-FROM layoffsproj.layoffsrawdata
-WHERE industry != ''
-ORDER BY YearLaidOff
-
